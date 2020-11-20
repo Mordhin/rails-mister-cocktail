d60e9fcade7a3b9f18e1a239004ae34ecd85cc0e
Julien Corion <astram59@gmail.com>
2020-11-20 16:53:56 +0100

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -15,13 +15,22 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
+    
     @cocktail.save
+    
     redirect_to cocktail_path(@cocktail)
   end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+  
 
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
